2a45afbe42b80da115e52aa82963c47dc24d6e8f
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-07-17 19:08:21 -0700

    updated js

---
+++ 31-repeated_word/repeated_word.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let string = 'Hi this string  will have a repeating word at some point in the string.'
+
+const repeatedWord = (str) => {
+  let map = {};
+  let output = null;
+  string = str.replace(/\./g,' ')
+  console.log(string)
+  let split = string.split(' ')
+
+  let i = 0;
+  while(output === null){
+    typeof map[split[i]] === 'undefined' ? map[split[i]] = 1 : map[split[i]]++;
+    console.log(map)
+    map[split[i]] === 2? output = split[i] : i++;
+  }
+  console.log(output)
+}
+
+repeatedWord(string);
